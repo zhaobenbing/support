4a8788299e2de58d32cc09f43e7af848b12ef9f4
zhaobenbing <zhaobenbing@wanda.cn>
2016-06-26 22:32:32 +0800

    init file

---
+++ app/src/main/java/com/support/MainActivity.java
@@ -0,0 +1,13 @@
+package com.support;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(com.support.R.layout.activity_main);
+  }
+}
